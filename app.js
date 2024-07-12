82cf0fcfc5951d53e0e697f125fab70ccfd3a013
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-07-12 23:31:31 +0530

    link changes

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const app = express();
 const { connection } = require("./connection");
 const { authRouter } = require("./router/auth-route");
 const { userRouter } = require("./router/user-route");
+
 connection();
-app.use(
-  cors({
-    origin: ["https://myntra-frontend-tau.vercel.app/"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  })
-);
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://myntra-frontend-c9345icmh-pankaj-parihars-projects.vercel.app",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cors());
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
