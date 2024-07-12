51674ec9db069e3ab7af6b4cf27ec14ebe5b7577
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-07-12 20:32:21 +0530

    app.js

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const { authRouter } = require("./router/auth-route");
 const { userRouter } = require("./router/user-route");
 connection();
 app.use(cors());
-app.use(express.json({
-  origin: ["https://myntra-backend-rho.vercel.app"],
-  methods: ["POST","GET"],
-  credentials:true
-}));
+app.use(
+  express.json({
+    origin: ["https://myntra-clone-api-jade.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 
