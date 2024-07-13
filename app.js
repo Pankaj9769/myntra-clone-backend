890bedbc5687b51738381e2b659817062d8e30f6
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-07-13 19:01:19 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ connection();
 
 const corsOptions = {
   origin: [
+    "http://localhost:5173",
     "https://myntra-frontend-c9345icmh-pankaj-parihars-projects.vercel.app",
     "https://myntra-frontend-tau.vercel.app/",
   ],
